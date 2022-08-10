51c173a6398b286db90e7b8dcb7a4e1dea8141ba
Jeffery Bennett <meandmybadself@gmail.com>
2022-08-09 22:23:56 -0500

    Adds TV cleaning functionality

---
+++ README.md
@@ -0,0 +1,17 @@
+# Mediacrapolis Cleaner
+
+## Purpose
+Keep Mediacrapolis from running out of disk space.
+
+## Changelog
+
+### 8/9/22
+* Updated movie cleaning functionality.
+* Experimenting with different types of television series deletion methods.
+
+## Related URLs
+
+## TODOs
+* Allow interactions via Slack
+    * Add new user
+    * Request media
--- index.js
+++ index.js
@@ -5,11 +5,12 @@ import cleanTV from './lib/tv/index.js'
 import { resyncOmbiMedia } from './lib/apis/ombi.js'
 
 async function run() {
- await cleanMovies()
- await cleanTV()
- 
- // Media resync applies to both movies and TV.
- await resyncOmbiMedia()
+
+    // await cleanMovies()
+    await cleanTV()
+
+    // Media resync applies to both movies and TV.
+    // await resyncOmbiMedia()
 }
 
 run()
